0e6c7d67bbf69de9045a709771140395646344c9
KT-Russell <kamran.russell97@gmail.com>
2025-12-06 10:49:36 +0000

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "vpc_cidr" {
 variable "public_subnets" {
   type        = list(string)
   description = "List of public subnet CIDRs"
-  default = [ ["10.0.1.0/24", "10.0.2.0/24"] ]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "tags" {
